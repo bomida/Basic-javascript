da1721eec550826114186b6abb5c136216f6a82b
BomiDo <bm.do.bomhada@gmail.com>
2021-11-02 23:26:38 +0900

    EDIT

--- JS 100제/JS 100.js
+++ JS 100제/JS 100.js
@@ -41,3 +41,18 @@ console.log(a+b);
 // 4)  _age
 // 5)  1age
 
+// answer : 3, 5번 - JavaScript 식별자는 문자, 밑줄(_) 혹은 달러 기호($)로 시작해야하며 let 은 이미 JavaScript 문법에 존재하는 예약어라 사용이 불가능
+
+// 8. 객체의 키 이름 중복
+// 자바스크립트 객체를 다음과 같이 만들었다. 출력값을 입력하시오. (출력값은 공백을 넣지 않습니다.)
+
+var d = {
+  'height':180,
+  'weight':78,
+  'weight':84,
+  'temperature':36,
+  'eyesight':1
+};
+
+console.log(d['weight']);
+// answer : 84 - 제일 마지막 값이 출력된다.
