a8d16c2f1db422de2ab6ad657e8c2357acf7e3dd
BomiDo <bm.do.bomhada@gmail.com>
2021-11-02 16:11:39 +0900

    EDIT

--- 기본 문법/04. operator_if_for loof.js
+++ 기본 문법/04. operator_if_for loof.js
@@ -105,3 +105,69 @@ console.log('' == false); //t
 console.log('' === false); //f
 console.log(null == undefined); //t
 console.log(null === undefined); //다른 타입이기 때문에 f
+
+// 8. Conditional operators: if
+// if, else if, else
+const name = 'ellie';
+if (name === 'ellie') {
+  console.log('Welcome, Ellie');
+} else if (name === 'coder') {
+  console.log('You are amazing coder');
+} else {
+  console.log('unknown');
+}
+
+// 9. Ternary operator: ?
+// condition ? value1 : value2;
+console.log(name === 'ellie' ? 'yes' : 'no');
+
+// 10. Switch statement
+// use for multiple if checks
+// use for enum-like value check
+// use for multiple type checks in TS
+const browser = 'IE';
+switch (browser) {
+  case 'IE':
+    console.log('go away!');
+    break;
+  case 'Chrome':
+  case 'Firefox':
+    console.log('love you!');
+    break;
+  default:
+    console.log('same all');
+    break;
+}
+
+// 11. Loops
+// while loop, while the condition is truthy,
+// body code is executed.
+let i = 3;
+while (i > 0) {
+  console.log(`while: ${i}`);
+  i--;
+}
+
+// do while loop, body code is executed first,
+// then check the codition.
+do {
+  console.log(`do while: ${i}`);
+  i--;
+} while (i > 0);
+
+// for loop, for(begin; condition; step)
+for (i = 3; i > 0; i--) {
+  console.log(`for: ${i}`);
+}
+
+for (let i = 3; i > 0; i = i - 2) {
+  // inline variable declaration
+  console.log(`inline variable for: ${i}`);
+}
+
+// nested loops
+for (let i = 0; i < 10; i++) {
+  for (let j = 0; j < 10; j++) {
+    console.log(`i: ${i}, j: ${j}`);
+  }
+}
