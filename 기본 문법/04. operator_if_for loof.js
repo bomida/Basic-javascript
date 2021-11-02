1610a7ab136552d584e33c85c715536ebca0bba8
BomiDo <bm.do.bomhada@gmail.com>
2021-11-02 15:31:31 +0900

    EDIT

--- 기본 문법/04. operator_if_for loof.js
+++ 기본 문법/04. operator_if_for loof.js
@@ -1,1 +1,17 @@
 'use strict';
+
+// 1. String concatenation
+console.log('my' + 'cat');
+console.log('1' + 2);
+console.log(`string literals: 1 + 2 = ${1 + 2}`);
+console.log('ellie\'s book');
+console.log("ellie's\nbook");
+console.log("ellie's\n\tbook");
+
+// 2. Numeric operators
+console.log(1 + 1); // add
+console.log(1 - 1); // substract
+console.log(1 / 1); // divide
+console.log(1 * 1); // multiply
+console.log(5 % 2); // remainder
+console.log(2 ** 3); // exponentiation
