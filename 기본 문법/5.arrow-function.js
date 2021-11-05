a677bacb10891bfd73816744a0790caec6da2828
BomiDo <bm.do.bomhada@gmail.com>
2021-11-05 22:56:31 +0900

    EDIT

--- 기본 문법/5.arrow-function.js
+++ 기본 문법/5.arrow-function.js
@@ -14,12 +14,12 @@
 
 function printHello() {
   console.log('Hello');
-}
+};
 printHello();
 
 function log(message) {
   console.log(message);
-}
+};
 log('Hello!');
 log(123);
 
@@ -28,7 +28,7 @@ log(123);
 // object parameters: passed by reference
 function changeName(obj){
   obj.name = 'coder';
-}
+};
 const bomi = { name: 'bomi' };
 changeName(bomi);
 console.log(bomi);
@@ -36,7 +36,7 @@ console.log(bomi);
 // 3. Default parameters (added in ES6)
 function showMessage(message, from = 'unknown') {
   console.log(`${message} by ${from}`);
-}
+};
 showMessage('Hi!');
 
 // 이전에는 인자의 값이 전달되지 않을 경우를 대비해 if문을 써서 입력되지않은 인자 값에 undefined가 출력되도록 직적 설정했었지만 from이라는 인자 값 옆에 원하는 default값을 지정해 놓으면 된다.
@@ -52,7 +52,7 @@ function printAll(...args) {
   }
 
   args.forEach((arg) => console.log(arg));
-}
+};
 printAll('earth', 'mercury', 'mars', 'venus');
 
 // ...args: rest parameters - 배열 형태로 전달
@@ -65,13 +65,13 @@ function printMessage() {
   console.log(message); // local variable
   console.log(globalMessage);
   // return undefined; // 생략 가능
-}
+};
 printMessage();
 
 // 6. Return a value
 function sum(a, b) {
   return a + b;
-}
+};
 const result = sum(1, 2); // 3
 console.log(`sum: ${sum(1, 2)}`);
 
@@ -81,7 +81,7 @@ function upgradeUser(user) {
   if (user.point > 10) {
     // long upgrade logic...
   }
-}
+};
 
 // good way
 function upgradeUser(user) {
@@ -89,7 +89,7 @@ function upgradeUser(user) {
     return;
   }
   // long upgrade logic...
-}
+};
 
 
 // First-class function
@@ -103,7 +103,7 @@ function upgradeUser(user) {
 // a function expression is created when the execution reaches it.
 const print = function () { // anonymous function: 이름이 없는 함수
   console.log('print');
-}
+};
 print(); // first print
 const printAgain = print;
 printAgain(); // second print
@@ -117,17 +117,39 @@ function randomQuiz(answer, printYes, printNo) {
   } else {
     printNo();
   }
-}
+};
 // anonymouse function
 const printYes = function () {
   console.log('YES!');
-}
+};
 
 // named function
 // better debugging in debugger's stack traces
 // recursions
 const printNo = function print() {
   console.log('NO:(');
-}
+};
 randomQuiz('wrong', printYes, printNo);
 randomQuiz('love you', printYes, printNo);
+
+// Arrow function
+// always anonymous
+
+// const simplePrint = function () {
+//   console.log('simplePrint');
+// };
+
+const simplePrint = () => console.log('simplePrint');
+const add = (a, b) => a + b;
+console.log(add(3, 3));
+
+// IIFE: Immediately Invoked Function Expression
+
+// function hello() {
+//   console.log('IIFE');
+// }
+// hello();
+
+(function hello() {
+  console.log('IIFE');
+})();
