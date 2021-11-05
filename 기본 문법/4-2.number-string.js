a46ee33114c77fbc8d47050f81e938182db68b02
BomiDo <bm.do.bomhada@gmail.com>
2021-11-05 15:11:16 +0900

    EDIT

--- 기본 문법/4-2.number-string.js
+++ 기본 문법/4-2.number-string.js
@@ -73,6 +73,10 @@ Math.floor // 내림
 Math.round // 반올림
 Math.trunc // 소수점 아래 잘라내기
 
+let randomNum = Math.random()*10;
+let totalNum = Math.trunc(randomNum, 2);
+console.log(totalNum);
+
 // 최대값, 최소값
 Math.max
 Math.min
