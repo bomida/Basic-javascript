bd91aa9c428c01491bb0f14584d421ccf38840d9
BomiDo <bm.do.bomhada@gmail.com>
2021-11-09 16:29:19 +0900

    EDIT

--- 기본 문법/4-4.control-flow-statement.js
+++ 기본 문법/4-4.control-flow-statement.js
@@ -26,6 +26,32 @@ function game() {
 }
 // game();
 
-if (result === 6) {
-  alert(`당신은 운이 좋군요`);
+// 다음과 같이 else가 필요 없는 경우에는 else를 생략할 수 있다.
+// if (result === 6) {
+//   alert(`당신은 운이 좋군요`);
+// };
+// if (result === 6) alert(`당신은 운이 좋군요`);
+
+// if...else 구문의 중첩
+// 만약 세 개 이상의 경우의 수를 if...else를 통해 표현하려면, if...else를 중첩 시키면 된다.
+// 위 코드를 가지고 주사위를 굴려 3또는 4가 나오면 비긴 것으로 게임을 고쳐보겠습니다.
+function roll() {
+  return Math.ceil(Math.random()*6);
 }
+
+function game() {
+  const result = roll();
+  alert(`결과: ${result}`);
+
+  if (result >= 5) {
+    alert(`당신이 이겼습니다.`);
+  } else {
+    // 중첩된 if...else 구문
+    if (result >= 3) {
+      alert(`비겼습니다.`);
+    } else {
+      alert(`당신이 졌습니다.`);
+    }
+  }
+}
+game();
