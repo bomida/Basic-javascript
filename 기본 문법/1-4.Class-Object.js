002735bd1c4a114d81613d96d77cd4aedf4399a3
BomiDo <bm.do.bomhada@gmail.com>
2021-11-10 21:49:23 +0900

    EDIT

--- 기본 문법/1-4.Class-Object.js
+++ 기본 문법/1-4.Class-Object.js
@@ -1,1 +1,9 @@
 'use strict';
+
+// Object-oriendted programing
+// classs: template
+// object: instance of a class
+// Javascript classes
+// - introduced in ES6
+// - syntactival sugar over prototype-based ingeritance
+
