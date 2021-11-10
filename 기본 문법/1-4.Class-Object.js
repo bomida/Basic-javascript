80f24df6d774331e97ea4cdbf39bf808105d23e3
BomiDo <bm.do.bomhada@gmail.com>
2021-11-10 22:41:45 +0900

    EDIT

--- 기본 문법/1-4.Class-Object.js
+++ 기본 문법/1-4.Class-Object.js
@@ -61,7 +61,7 @@ console.log(experiment.privateField);
 // 4. Static properties and methods
 // Too soon!
 class Article {
-  static publisher = 'Dream Coding';
+  static publisher = 'Hello World';
   constructor(articleNumber) {
     this.articleNumber = articleNumber;
   }
@@ -76,3 +76,39 @@ const article1 = new Article(1);
 const article2 = new Article(2);
 console.log(Article.publisher);
 Article.printPublisher();
+
+// 5. Inheritance
+// a way for one class to extend another class.
+class Shape {
+  constructor(width, height, color) {
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+
+  draw() {
+    console.log(`drawing ${this.color} color!`);
+  }
+
+  getArea() {
+    return this.width * this.height;
+  }
+}
+
+class Rectangle extends Shape {}
+class Triangle extends Shape {
+  draw() {
+    super.draw();
+    console.log('🔺');
+  }
+  getArea() {
+    return (this.width * this.height) / 2;
+  }
+}
+
+const rectangle = new Rectangle(20, 20, 'blue');
+rectangle.draw();
+console.log(rectangle.getArea());
+const triangle = new Triangle(20, 20, 'red');
+triangle.draw();
+console.log(triangle.getArea());
