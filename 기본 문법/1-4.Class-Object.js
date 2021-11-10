001db72c4548c9622826f543bceaf835c5323619
BomiDo <bm.do.bomhada@gmail.com>
2021-11-10 22:05:07 +0900

    EDIT

--- 기본 문법/1-4.Class-Object.js
+++ 기본 문법/1-4.Class-Object.js
@@ -1,9 +1,26 @@
 'use strict';
 
 // Object-oriendted programing
-// classs: template
+// class: template
 // object: instance of a class
 // Javascript classes
 // - introduced in ES6
-// - syntactival sugar over prototype-based ingeritance
+// - syntactival sugar over prototype-based inheritance
 
+// 1. Class declarations
+class Person {
+  // constructor
+  constructor(name, age) {
+    // fields
+    this.name = name;
+    this.age = age;
+  }
+  // methods
+  speak() {
+    console.log(`${this.name} hello`);
+  }
+}
+const bomi = new Person(`bomi`, 20);
+console.log(bomi.name);
+console.log(bomi.age);
+bomi.speak();
