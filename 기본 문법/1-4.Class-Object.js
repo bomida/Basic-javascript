60d6a88816c374cc0606e51c8459ed270144e1d7
BomiDo <bm.do.bomhada@gmail.com>
2021-11-10 22:23:56 +0900

    EDIT

--- 기본 문법/1-4.Class-Object.js
+++ 기본 문법/1-4.Class-Object.js
@@ -24,3 +24,26 @@ const bomi = new Person(`bomi`, 20);
 console.log(bomi.name);
 console.log(bomi.age);
 bomi.speak();
+
+// 2. Getter and Setters
+class User {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  get age() { // 값을 리턴
+    return this._age;
+  }
+  set age(value) { // 값을 설정 > 대신에 값을 받아와야함
+    // if (value < 0) {
+    //   throw Error('age can not be negative')
+    // }
+    this._age = value < 0 ? 0 : value;
+  }
+}
+
+const user1 = new User('Steve', 'Job', -1);
+console.log(user1.age);
+
