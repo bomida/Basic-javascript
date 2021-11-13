5657f40384c4c967e7ca5253e0adbdbfffcbf83a
BomiDo <bm.do.bomhada@gmail.com>
2021-11-13 22:26:18 +0900

    EDIT

--- 기본 문법/1-5.object.js
+++ 기본 문법/1-5.object.js
@@ -5,18 +5,34 @@
 // a collection of related data and/or functionality.
 // Nearly all objects in Javascript are instances of Object
 
-// object = {key: value};
+// object = {key: value}; // 오브젝트는 key와 value의 집합체이다.
 
+// 1. Literals and properties
+/*
 const name = 'bomi';
-const age = 4;
+const age = 20;
 print(name, age);
-// function print(name, age) {
-//   console.log(name);
-//   console.log(age);
-// }
+*/
+const obj1 = {}; // 'object literal' syntax
+const obj2 = new Object(); // 'object constructor' syntax
+
 function print(person) {
   console.log(person.name);
   console.log(person.age);
 }
 const bomi = {name: 'bomi', age: 20};
 print(bomi);
+
+// with Javascript magic (dynamically typed language)
+// can add properties later
+bomi.hasJob = true;
+console.log(bomi.hasJob);
+
+// can delete properties later
+delete bomi.hasJob;
+console.log(bomi.hasJob);
+
+
+// 2. Computed properties
+console.log(bomi.name);
+console.log(bomi['name']);
--- 기본 문법/2-5.object.js
+++ 기본 문법/2-5.object.js
@@ -6,8 +6,12 @@
 // 객체는 한꺼번에 여러 값을 담을 수 있는 container과 같은 자료구조(data structure)이다.
 // 객체 안에는 이름-값 쌍(name-value pair)이 저장되는데, 이를 객체의 속성(property)라고 합니다. - 중괄호 안에 이름-값 쌍을 적어주면 된다.
 const person = {
-  name: '보미', // 속성 이름 - 'name', 속성 값 - '보미'
-  age: 19, // 속성 이름 - 'age', 속성 값 - 19
-  'languages': ['korean', 'English'], // 속성 이름 - 'languages', 속성 값 - 배열
-  '한국 나이': 20 // 속성 이름 - 'age', 속성 값 - 20
+  name: '보미',
+  // 속성 이름 - 'name', 속성 값 - '보미'
+  age: 19,
+  // 속성 이름 - 'age', 속성 값 - 19
+  'languages': ['korean', 'English'],
+  // 속성 이름 - 'languages', 속성 값 - 배열
+  '한국 나이': 20
+  // 속성 이름 - 'age', 속성 값 - 20
 };
