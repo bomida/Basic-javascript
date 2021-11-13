6194e40755f3e12ce470c24553f15678ec843a6e
BomiDo <bm.do.bomhada@gmail.com>
2021-11-13 22:34:14 +0900

    EDIT

--- 기본 문법/1-5.object.js
+++ 기본 문법/1-5.object.js
@@ -34,5 +34,19 @@ console.log(bomi.hasJob);
 
 
 // 2. Computed properties
+// key should be always string
+// .(dot) : 코딩하는 그 순간 그 키에 해당하는 값을 받아오고 싶을 때
+// [] : 정확하게 어떤키가 필요한지 모를 때 즉, 런타임에서 결정될 때 사용 -> 실기간으로 원하는 키의 값을 받아오고 싶아면 확인용으로 주로 씀.
 console.log(bomi.name);
 console.log(bomi['name']);
+bomi['hasJob'] = true;
+console.log(bomi.hasJob);
+
+function printValue(obj, key) {
+  console.log(obj[key]);
+}
+printValue(bomi, 'name');
+printValue(bomi, 'age');
+
+// 3. Property value shorthand
+// 다른 계산을 하지 않고 순수하게 오브젝트를 생성하는 함수들의 네이밍은 대문자로 시작
