81594da20a3afcc20ef361622a7fa7421bd417bf
BomiDo <bm.do.bomhada@gmail.com>
2021-11-14 21:32:26 +0900

    EDIT

--- 기본 문법/1-6.array-apis.js
+++ 기본 문법/1-6.array-apis.js
@@ -1,1 +1,30 @@
 'use strict';
+
+// Array 🥳
+
+// 1. Declaration
+const arr1 = new Array();
+const arr2 = [1, 2];
+
+// 2. Index position
+const fruits = ['🍎', '🍌'];
+console.log(fruits);
+console.log(fruits.length);
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[fruits.length - 1]); // 배열의 마지막 아이템을 찾을 때에는
+
+// 3. Looping over an array
+// print all fruits
+// a. for
+for(let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
+
+// b. for of
+for (let fruit of fruits) {
+  console.log(fruit);
+}
+
+// e. forEach
+fruits.forEach((fruits) => console.log(fruits));
