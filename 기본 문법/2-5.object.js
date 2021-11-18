ca8059aeed12c2abcabf6eca8558e5dad50fb21d
BomiDo <bm.do.bomhada@gmail.com>
2021-11-19 01:51:23 +0900

    EDIT

--- 기본 문법/2-5.object.js
+++ 기본 문법/2-5.object.js
@@ -229,8 +229,75 @@ console.groupEnd('프로토타입 (Prototype)');
 
 
 console.group('생성자 (Constructor)');
+// new 키워드가 붙었다는 것 말고는 함수 호출 문법과 비슷하게 생겼지만 사실은 object는 함수이다. 이렇게 객체를 만들 때 new 키워드와 함께 사용하는 함수를 가지고 생성자(constructor)라고 부른다.
+{
   const obj = new Object();
-  // 위 문장은 new 키워드가 붙었다는 것 말고는 함수 호출 문법과 비슷하게 생겼지만 사실은 object는 함수이다. 이렇게 객체를 만들 때 new 키워드와 함께 사용하는 함수를 가지고 생성자(constructor)라고 부른다.
+  console.log(`typeof Object: ${typeof Object}`);
+}
 
-  const obj = new Object();
+//생성자 정의하기
+// JS에서는 Object 뿐만아니라, 내장된 많은 생성자들이 있고, 심지어 프로그래머가 직접 생성자를 만들 수도 있다.
+{
+  // 생성자 정의
+  function Person(name) {
+    this.name = name;
+  }
+  
+  // 생성자를 통한 객체 생성
+  const person1 = new Person('도보미');
+
+// 인스턴스 (Instance)
+// 생성자를 통해 생성된 객체를 그 생성자의 인스턴스(instance)라고 한다. 위의 예제에서는 person1이 Person의 인스턴스 이다. instanceof 연산자를 사용하면, 객체가 특정 생성자의 인스턴스가 맞는지 확인할 수 있다.
+
+  console.log(`instanceof: ${person1 instanceof Person}`);
+  
+  // 깨체 리터럴을 통해 생성된 객체는 Object의 인스턴스이다.
+  const obj = {};
+  console.log(`instanceof: ${obj instanceof Object}`);
+
+// 생성자와 프로토타입
+// 생성자를 통해 만들어낸 객체의 프로토타입에는 생성자의 prototyp 속성에 저장되어 있는 객체가 자동으로 지정된다.
+
+  console.log(Object.getPrototypeOf(person1) === Person.prototype);
+  // 이때, Person.prototype에 객체를 저장한 적이 없고, Person은 함수인데도 불구하고 속성을 갖는 이유는 함수도 특별한 형태의 객체이기 때문이다.
+  // JS에서는 function 구문을 통해 함수를 정의할 때 함수의 prototype 속성에 객체가 자동으로 생성되어 저장되기 때문
+} {
+  function Person() {
+    // ...
+  }
+  console.log(typeof Person.prototype);
+}
+
+// constructor
+// 생성자의 prototype속성에 자동 생성되는 객체에는 constructor라는 특별한 속성이 들어있다. 이 속성에는 생성자 자신이 저장된다.
+
+{
+  function Person() {
+    // ...
+  }
+  console.log(Person.prototype.constructor === Person);
+}
+
+// 이를 통해, 어떤 객체가 어떤 생성자로부터 생성되었는지를 constructor 속성을 통해 알아낼 수 있다.
+{
+  function Person() {
+    // ...
+  }
+  const person = new Person();
+  console.log(person.constructor === Person);
+}
+
+// 팩토리 함수의 재작성
+{
+  // 사람을 나타내는 객체를 생성하는 팩토리 함수
+  function Person(name) {
+    this.name = name;
+  }
+  Person.prototype.introduce = function() {
+    return `안녕하세요, 제 이름은 ${this.name}입니다.`;
+  }
+
+  const person = new Person('도보미');
+  console.log(`person.introduce: ${person.introduce()}`);
+}
 console.groupEnd('생성자 (Constructor)');
