4b71c356b04809079ec77117a4902275ad48c364
BomiDo <bm.do.bomhada@gmail.com>
2021-11-20 16:08:31 +0900

    EDIT

--- 기본 문법/1-4.Class-Object.js
+++ 기본 문법/1-4.Class-Object.js
@@ -17,13 +17,15 @@ class Person {
   }
   // methods (행동)
   speak() {
-    console.log(`${this.name}, hello`);
+    console.log(`Hello, ${this.name}`);
   }
 }
 const bomi = new Person(`bomi`, 20); // 새로운 오브젝트를 만들 때에는 new라는 키워드 사용
+console.log(bomi);
 console.log(bomi.name);
 console.log(bomi.age);
 bomi.speak();
+
 
 // 2. Getter and Setters
 class User {
@@ -46,6 +48,7 @@ class User {
 const user1 = new User('Steve', 'Job', -1);
 console.log(user1.age);
 
+
 // 3. Fields (public, private);
 // Too soon!
 
@@ -56,6 +59,7 @@ class Experiment {
 const experiment = new Experiment();
 console.log(experiment.publicField);
 console.log(experiment.privateField);
+
 
 // 4. Static properties and methods
 // Too soon!
@@ -74,25 +78,31 @@ const article2 = new Article(2);
 console.log(Article.publisher);
 Article.printPublisher();
 
+
 // 5. Inheritance
 // a way for one class to extend another class.
+console.group('Inheritance');
 class Shape {
   constructor(width, height, color) {
     this.width = width;
     this.height = height;
     this.color = color;
   }
-
+  
   draw() {
-    console.log(`drawing ${this.color} color of`)
+    console.log(`drawing ${this.color} color of`);
   }
-
+  
   getArea() {
     return this.width * this.height;
   }
 }
 
 class Rectangle extends Shape {}
+const rectangle = new Rectangle(20, 20, '🔵');
+console.log(`rectangle.draw(): ${rectangle.draw()}`);
+console.log(`rectangle.getArea(): ${rectangle.getArea()}`);
+
 class Triangle extends Shape {
   draw() {
     super.draw();
@@ -106,13 +116,10 @@ class Triangle extends Shape {
     return `Triangle: color: ${this.color}`;
   }
 }
-
-const rectangle = new Rectangle(20, 20, 'blue');
-rectangle.draw();
-console.log(rectangle.getArea());
-const triangle = new Triangle(20, 20, 'red');
-triangle.draw();
-console.log(triangle.getArea());
+const triangle = new Triangle(20, 20, '🔴');
+console.log(`triangle.draw(): ${triangle.draw()}`);
+console.log(`triangle.getArea(): ${triangle.getArea()}`);
+console.groupEnd('Inheritance');
 
 // 6. Class checking: instanceOf
 console.log(rectangle instanceof Rectangle); // t
