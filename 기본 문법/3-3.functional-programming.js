fb968d7900d13b7fac2bd5c6850c4bbc92f25491
BomiDo <bm.do.bomhada@gmail.com>
2021-11-22 00:38:31 +0900

    EDIT

--- 기본 문법/3-3.functional-programming.js
+++ 기본 문법/3-3.functional-programming.js
@@ -141,6 +141,7 @@
 
 // 루프와 재귀 함수
 // 대부분의 루프는 재귀 함수를 통해 다시 구현될 수 있다.
+// 코드의 의미가 명확해지고 코드의 길이를 줄일 수 있다는 장점이 있다.
 {
   // 루프로 구현된 팩토리얼
   function factorialLoop(n) {
@@ -180,4 +181,72 @@
     );
   }
   console.log('fiboRec:', fiboRec(4));
+}
+
+
+// 분할 정복 (Divide and Conquer)
+// 분할 정복(divide and conquer)은 문제를 작은 부분 문제로 나누어서 푼 뒤, 그 결과를 합치는 식으로 알고리즘을 작성하는 기법이다.
+// fiboRec 역시 분할 정복의 일종이라 할 수 있다.
+
+// 분할 정복 기법을 활용하는 알고리즘 중 대표적인 예로 병함 정렬(merge sort)가 있다.
+{
+  function mergeSort(arr) {
+    // 입력 된 배열의 길이가 1 이하면 더 이상 재귀 호출을 하지 않습니다.
+    if (arr.length <= 1) return arr;
+
+    // 배열을 절반으로 잘라 두 개의 작은 배열로 분할하고, 두 작은 배열에 대해 재귀 함수를 수행한다.
+    const slicer = Math.floor(arr.length / 2);
+    const arr1 = mergeSort(arr.slice(0, slicer));
+    const arr2 = mergeSort(arr.slice(slicer));
+
+    // `arr1`, `arr2`는 **이미 정렬되어있는 상태**이므로,
+    // 이 성질을 이용해 두 배열을 **정렬되어있는 큰 배열**로 합칠 수 있다.
+    const newArr = [];
+    for (let i = 0, j = 0; i < arr1.length || j < arr2.length; ) {
+      if (arr1[i] == undefined || arr1[i] > arr2[j]) {
+        newArr.push(arr2[j]);
+        j++;
+      } else {
+        newArr.push(arr1[i]);
+        i++;
+      }
+    }
+  
+    // 큰 배열을 반환한다.
+    return newArr;
+  }
+}
+
+// 주의할 점
+// 재귀 함수가 실행되는 동안에는 종료되지 않은 함수가 아주 많이 생기게 되므로, 컴퓨터의 메모리에 큰 부담을 줄 수 있다.
+// JS 구동 환경에서는 특정 깊이 이상의 재귀 호출이 일어날 수 없도록 제한을 두고 있다.
+// Chrome 브라우저의 경우 대량 10000번 정도의 재귀 호출이 일어나면 아래와 같은 에러를 발생시킨다.
+{
+  // factorialRec(20000);
+  // RangeError: Maximum call stack size exceeded
+}
+{
+  const fiboRecMemoized = (() => {
+    // 계산 결과를 저장하는 저장소를 만듭니다.
+    const memo = new Map();
+  
+    const fiboRec = n => {
+      // 만약에 이전에 같은 인수로 계산한 적이 있다면
+      // 해당 결과를 바로 반환합니다.
+      let result = memo.get(n);
+      if (result != undefined) return result;
+  
+      result = (
+        n < 1 ? 0 :
+        n === 1 ? 1 :
+        fiboRec(n - 1) + fiboRec(n - 2)
+      );
+  
+      // 계산 결과를 저장소에 저장한 후 반환합니다.
+      memo.set(n, result);
+      return result;
+    }
+  
+    return fiboRec;
+  })();
 }
