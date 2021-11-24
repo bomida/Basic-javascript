7e4af3008732be712d8c9884c6402d9114949a2d
BomiDo <bm.do.bomhada@gmail.com>
2021-11-25 01:06:11 +0900

    EDIT

--- 기본 문법/1-9.callback.js
+++ 기본 문법/1-9.callback.js
@@ -1,2 +1,20 @@
 'use strict';
 
+// Javascript is synchronous.
+// Excute the code block by orger ater hoisting.
+// hoisting: var, function declaration
+console.log('1');
+setTimeout(() => console.log('2'), 1000);
+console.log('3');
+
+// Synchronous callback
+function printImmediately(print) {
+  print();
+}
+printImmediately(() => console.log('hello'));
+
+// Asynchronous callback
+function printWithDelay(print, timeout) {
+  setTimeout(print, timeout);
+}
+printWithDelay(() => console.log('async callback'), 2000);
