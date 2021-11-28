16fa7595dbe01daf6c547399cc9dd8d1ec25e54c
BomiDo <bm.do.bomhada@gmail.com>
2021-11-28 19:09:57 +0900

    EDIT

--- 기본 문법/4-2.class.js
+++ 기본 문법/4-2.class.js
@@ -69,3 +69,54 @@ console.groupEnd('class'); }
 // - 클래스는 함수로 호출될 수 없다.
 // - 클래스 선언은 let과 const처럼 블록 스코프에 선언되며, 호이스팅(hoisting)이 일어나지 않는다.
 // - 클래스의 메소드 안에서 super 키워드를 사용할 수 있다.
+
+
+// 메소드 정의하기
+// 클래스의 메소드를 정의할 때는 객체 리터럴에서 사용하던 문법과 유사한 문법을 사용한다.
+// 인스턴스 메소드(instance method)는 다음과 같은 문법을 통해 정의한다.
+{
+  class Calculator {
+    add(x, y) {
+      return x + y;
+    }
+    subtrack(x, y) {
+      return x - y;
+    }
+  }
+  console.log(new Calculator().add(2, 3));
+}
+
+// 객체 리터럴의 문법과 마찬가지고, 임의의 표현식을 대괄호로 둘러싸서 메소드의 이름으로 사용할 수도 있다.
+{
+  const methodName = 'introduce';
+  class Person {
+    constructor({name, age}) {
+      this.name = name;
+      this.age = age;
+    }
+    // 아래 메소드의 이름은 `introduce`가 된다.
+    [methodName]() {
+      return `Hello, I am ${this.name} :)`;
+    }
+  }
+  console.log(new Person({name: 'bomi', age: 20}).introduce());
+}
+
+// Getter 혹은 Setter를 정의하고 싶을 때는 메소드 이름 앞에 get 또는 set을 붙여주면 된다.
+{
+  class Account {
+    constructor() {
+      this._balance = 0;
+    }
+    get balance() {
+      return this._balance;
+    }
+    set balance(newBalance) {
+      this._balance = newBalance;
+    }
+  }
+
+  const account = new Account();
+  account.balance = 1000;
+  console.log('account.balance: ' + account.balance);
+}
